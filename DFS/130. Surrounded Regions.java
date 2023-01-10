94c61d72be35918f7f9e90e481974c4450738d34
Yash Poonia <93772907+yash7xm@users.noreply.github.com>
2023-01-10 21:45:03 +0530

    Create 130. Surrounded Regions.java

---
+++ DFS/130. Surrounded Regions.java
@@ -0,0 +1,35 @@
+class Solution {
+    public static void dfs(char[][] board, int i, int j){
+        if(i<0 || j<0 || i==board.length || j==board[0].length || board[i][j]!='O'){
+            return;
+        }
+        board[i][j]='#';
+        dfs(board,i-1,j);
+        dfs(board,i+1,j);
+        dfs(board,i,j-1);
+        dfs(board,i,j+1);
+    }
+    public void solve(char[][] board) {
+//        Searching in the rows
+        for(int i=0; i<board[0].length; i++){
+            if(board[0][i]=='O')
+                dfs(board,0,i);
+            if(board[board.length-1][i]=='O')
+                dfs(board,board.length-1,i);
+        }
+//       Searching in the cols
+        for(int i=0; i<board.length; i++){
+            if(board[i][0]=='O')
+                dfs(board,i,0);
+            if(board[i][board[0].length-1]=='O')
+                dfs(board,i,board[0].length-1);
+        }
+        for(int i=0; i<board.length; i++){
+            for(int j=0; j<board[0].length; j++){
+             if(board[i][j] == 'O') board[i][j] = 'X';   
+             if(board[i][j] == '#') board[i][j] = 'O';
+                 
+            }
+        }
+    }
+}
